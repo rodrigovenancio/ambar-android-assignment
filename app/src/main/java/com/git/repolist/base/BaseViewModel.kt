429fd7b12cb4cd115570b5054161dd8393de434a
Rodrigo Venancio <rodrigo.venancio@usp.br>
2020-08-05 15:34:20 -0300

    (+) base view model

--- app/src/main/java/com/git/repolist/base/BaseViewModel.kt
+++ app/src/main/java/com/git/repolist/base/BaseViewModel.kt
@@ -1,13 +1,10 @@
-package com.rodrigo.buxtrading.base
+package com.git.repolist.base
 
 import androidx.lifecycle.ViewModel
-import com.rodrigo.buxtrading.injection.module.NetworkModule
-import com.rodrigo.buxtrading.injection.component.DaggerViewModelInjector
-import com.rodrigo.buxtrading.injection.component.ViewModelInjector
-import com.rodrigo.buxtrading.ui.product.viewmodel.ProductViewModel
+import com.git.repolist.injection.module.NetworkModule
 
 abstract class BaseViewModel:ViewModel(){
-    private val injector: ViewModelInjector = DaggerViewModelInjector
+    /*private val injector: ViewModelInjector = DaggerViewModelInjector
         .builder()
         .networkModule(NetworkModule)
         .build()
@@ -16,12 +13,12 @@ abstract class BaseViewModel:ViewModel(){
         inject()
     }
 
-    /**
+    *//**
      * Injects the required dependencies
-     */
+     *//*
     private fun inject() {
         when (this) {
-            is ProductViewModel -> injector.inject(this)
+            is RepositoryListViewModel -> injector.inject(this)
         }
-    }
+    }*/
 }
