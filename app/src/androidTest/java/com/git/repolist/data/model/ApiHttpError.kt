97d63523202321c579ca7944bab8974e789db4a7
Rodrigo Venancio <rodrigo.venancio@usp.br>
2020-08-05 15:34:39 -0300

    (*) switched files to the right package

--- app/src/androidTest/java/com/git/repolist/data/model/ApiHttpError.kt
+++
@@ -1,7 +0,0 @@
-package com.rodrigo.buxtrading.data.model
-
-data class ApiHttpError(
-    val message: String,
-    val developerMessage: String,
-    val errorCode: String
-)
--- app/src/androidTest/java/com/git/repolist/data/model/Repository.kt
+++
@@ -1,7 +0,0 @@
-package com.rodrigo.buxtrading.data.model
-
-data class Product(
-    val id: String,
-    val name: String,
-    val symbol: String? = null
-)
